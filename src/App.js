8f6cac0ba512af9324d82e453e21faa8e4bc5c47
PRASHANTH18ps <psprashanth18@gmail.com>
2024-01-30 10:33:46 +0530

    Prashanth first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-// import logo from './logo.svg';
-// import './App.css';
+
 import React from 'react';
 import Forgotpassword from './components/Forgotpassword';
 import {BrowserRouter,Route,Routes} from"react-router-dom";
