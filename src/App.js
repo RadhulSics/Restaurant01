840d73155e2d40df46c520ffd33ba4f00363b4fe
Praveenps18 <praveenps832002@gmail.com>
2024-01-30 10:46:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import React from 'react';
 
 import Projectreg from "./component/Projectreg";
 import "./component/Style/Projectreg.css";
-// import Forgotpassword from './components/Forgotpassword';
+import Forgotpassword from './components/Forgotpassword';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path='/Forgotpassword'element={<Forgotpassword/>}/>
+      </Routes>
+        </BrowserRouter>
     <Projectreg/>
     
      
