e550ae04f6d4b72dbcf9232e6b58b2bcb57ebc96
Praveenps18 <praveenps832002@gmail.com>
2024-01-30 10:23:22 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 import Projectreg from "./component/Projectreg";
 import "./component/Style/Projectreg.css";
-
+// import Forgotpassword from './components/Forgotpassword';
 function App() {
   return (
     <div className="App">
