cbcbb059ee4ecea02eca4bd52ec84580a82e8cdb
Praveenps18 <praveenps832002@gmail.com>
2024-01-30 10:11:26 +0530

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 // // import logo from './logo.svg';
 // // import './App.css';
 import React from 'react';
-import Forgotpassword from './components/Forgotpassword';
-import {BrowserRouter,Route,Routes} from"react-router-dom";
-import React from "react";
+
 import Projectreg from "./Component/Projectreg";
 import "./Component/Style/Projectreg.css";
 
 function App() {
   return (
     <div className="App">
-..
+    <Projectreg/>
+    
+     
     </div>
   );
 }
