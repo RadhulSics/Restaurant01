839b90bef25acd0beb2d2be57705acec949e23a0
PRASHANTH18ps <psprashanth18@gmail.com>
2024-02-15 11:45:59 +0530

    Prashanth first commit

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           <Route path="/Customerviewmenu" element={<Customerviewmenu/>}/>
         </Routes>
       </BrowserRouter>
-      {/* Hai */}
+     
     </div>
   );
 }
